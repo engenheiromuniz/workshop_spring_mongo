5bceb4ed87ae939393a0b586313cbe75b80eb0a2
Engenheiro Muniz <engenheiro.muniz@gmail.com>
2025-10-02 22:57:47 -0300

    Obtendo usuário por ID.

--- src/main/java/com/muniz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/muniz/workshopmongo/resources/UserResource.java
@@ -1,19 +1,20 @@
 package com.muniz.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.muniz.workshopmongo.domain.User;
 import com.muniz.workshopmongo.dto.UserDTO;
 import com.muniz.workshopmongo.services.UserService;
+
+// REMOVA: import ch.qos.logback.core.joran.spi.HttpUtil.RequestMethod; // REMOVER ESTA LINHA!
 
 @RestController
 @RequestMapping("/users")
@@ -29,4 +30,10 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);		
 	}
 
+    // CORRIGIDO: Removido o 'method=RequestMethod.GET' e a importação incorreta
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id); 
+		return ResponseEntity.ok().body(new UserDTO(obj));		
+	}	
 }
--- src/main/java/com/muniz/workshopmongo/services/UserService.java
+++ src/main/java/com/muniz/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.muniz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.muniz.workshopmongo.domain.User;
 import com.muniz.workshopmongo.repositories.UserRepository;
+import com.muniz.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,15 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();		
+	}
+	
+	public User findById(String id) {
+		// Código CORRIGIDO/SIMPLIFICADO
+	    Optional<User> obj = repository.findById(id); 
+	    
+	    // O método orElseThrow() retorna o objeto User se ele estiver presente, 
+	    // ou lança a exceção fornecida se o Optional estiver vazio.
+	    return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 
 }
